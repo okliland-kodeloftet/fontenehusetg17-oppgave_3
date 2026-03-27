4e333f209687cf600a76bffef46635bf44cfe0e1
okliland-kodeloftet <okliland@hotmail.com>
2026-03-27 13:48:02 +0100

    ajaj

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+console.log('Hello world!');
+
+const hamburgerIcon = document.querySelector("hamburgerIcon");
+
+console.log(hamburgerIcon);
+
+
+
+hamburgerIcon.addEventListener("click", function(){
+    console.log("noe er klikket på")
+
+    console navList = document.querySelector("#navId");
+    console.log(navList);
+
+    navList.classList.toggle("navHamburgerList")
+});
